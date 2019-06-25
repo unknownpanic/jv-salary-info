a76d5dc4c143156cef386845208eb8372a3d859b
Sofasmile <sofiya.dyakonova@ukr.net>
2019-06-25 14:59:32 +0300

    renamed variables

--- src/test/java/core/basesyntax/SalaryInfoTest.java
+++ src/test/java/core/basesyntax/SalaryInfoTest.java
@@ -4,8 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SalaryInfoTest {
-    private static final String[] roles = {"Сергей", "Андрей", "София"};
-    private static final String[] scriptArray = {
+    private static final String[] ROLES = {"Сергей", "Андрей", "София"};
+    private static final String[] SCRIPT_ARRAY = {
             "25.04.2019 Сергей 60 50",
             "25.04.2019 Андрей 3 200",
             "25.04.2019 София 10 100",
@@ -17,13 +17,13 @@ public class SalaryInfoTest {
             "27.04.2019 София 3 80",
             "27.04.2019 Андрей 8 100"
     };
-    private static final String[] dates = {
+    private static final String[] DATES = {
             "24.04.2019",
             "25.04.2019",
             "26.04.2019",
             "27.04.2019"
     };
-    private static final String[] correctResult = {
+    private static final String[] EXPECTED_REPORTS = {
             "Отчёт за период 24.04.2019 - 24.04.2019\n" +
                     "Сергей - 0\n" +
                     "Андрей - 0\n" +
@@ -45,22 +45,22 @@ public class SalaryInfoTest {
     @Test
     public void getSalaryInfo() {
         SalaryInfo salary = new SalaryInfo();
-        for (int i = 0; i < dates.length; i++) {
-            String actualResult = salary.getSalaryInfo(roles, scriptArray, dates[0], dates[i]);
-            String expectedResult = correctResult[i];
+        for (int i = 0; i < DATES.length; i++) {
+            String actualResult = salary.getSalaryInfo(ROLES, SCRIPT_ARRAY, DATES[0], DATES[i]);
+            String expectedResult = SalaryInfoTest.EXPECTED_REPORTS[i];
 
             Assert.assertEquals(
-                    "Test failed from date " + dates[0] + " to " + dates[i]
+                    "Test failed from date " + DATES[0] + " to " + DATES[i]
                             + "\nactual: \n" + actualResult
                             + "\nexpected: \n" + expectedResult,
                     expectedResult,
                     actualResult);
         }
 
-        String actualResult = salary.getSalaryInfo(roles, scriptArray, dates[1], dates[0]);
+        String actualResult = salary.getSalaryInfo(ROLES, SCRIPT_ARRAY, DATES[1], DATES[0]);
         String expectedResult = null;
         Assert.assertEquals(
-                "Test failed from date " + dates[1] + " to " + dates[0]
+                "Test failed from date " + DATES[1] + " to " + DATES[0]
                         + "\nactual: \n" + actualResult
                         + "\nexpected: \n" + expectedResult,
                 expectedResult,
