e0031a509e123947cf5c5bde194436eafca2f63d
maxima <maxima.at.work@gmail.com>
2025-09-24 20:34:56 +0300

    minor checklist violations were fixed

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -5,31 +5,25 @@ public class SalaryInfo {
     private static final int INDEX_OF_HOURS = 2;
     private static final int INDEX_OF_NAME = 1;
     private static final int INDEX_OF_PAY_RATE = 3;
-    private final Utilities utilities = new Utilities();
-
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder stringBuilder = new StringBuilder();
         int[] salaries = new int[names.length];
         String[] localData;
-
         stringBuilder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
-
         for (int i = 0; i < data.length; i++) {
-            localData = utilities.parseLine(data[i]);
-            if (utilities.isDateInBound(dateFrom, dateTo, localData[INDEX_OF_DATE])) {
-                int indexOfName = utilities.indexOfName(names, localData[INDEX_OF_NAME]);
+            localData = Utilities.parseLine(data[i]);
+            if (Utilities.isDateInBound(dateFrom, dateTo, localData[INDEX_OF_DATE])) {
+                int indexOfName = Utilities.indexOfName(names, localData[INDEX_OF_NAME]);
                 if (indexOfName != -1) {
                     salaries[indexOfName] += Integer.parseInt(localData[INDEX_OF_HOURS])
                             * Integer.parseInt(localData[INDEX_OF_PAY_RATE]);
                 }
             }
         }
-
         for (int i = 0; i < names.length; i++) {
             stringBuilder.append(System.lineSeparator()).append(names[i])
                     .append(" - ").append(salaries[i]);
         }
-
         return stringBuilder.toString();
     }
 }
--- src/main/java/core/basesyntax/Utilities.java
+++ src/main/java/core/basesyntax/Utilities.java
@@ -6,12 +6,10 @@ import java.time.format.DateTimeFormatter;
 public class Utilities {
     private static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-    public String[] parseLine(String line) {
-        return line.split(" ");
+    public static String[] parseLine(String line) {
+        return line.trim().split("\\s+", 4);
     }
-
-    public int indexOfName(String[] names, String name) {
+    public static int indexOfName(String[] names, String name) {
         for (int i = 0; i < names.length; i++) {
             if (names[i].equals(name)) {
                 return i;
@@ -20,8 +18,7 @@ public class Utilities {
 
         return -1;
     }
-
-    public boolean isDateInBound(String dateFrom, String dateTo, String currentDate) {
+    public static boolean isDateInBound(String dateFrom, String dateTo, String currentDate) {
         LocalDate from = LocalDate.parse(dateFrom, DATE_FORMATTER);
         LocalDate to = LocalDate.parse(dateTo, DATE_FORMATTER);
         LocalDate current = LocalDate.parse(currentDate, DATE_FORMATTER);
